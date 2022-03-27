ced8ca8e40a5ea1f448dab35ae0ffa1d55f76e21
Cem Tuver <cemtuver@outlook.com>
2022-03-27 23:57:28 +0200

    Solve 75. Sort Colors

---
+++ 00/0075-Sort_Colors.kt
@@ -0,0 +1,39 @@
+class Solution {
+
+    fun sortColors(nums: IntArray): Unit {
+        var redIndex = 0
+        var blueIndex = nums.size - 1
+        var current = 0
+        
+        while (current <= blueIndex) {
+            when (nums[current]) {
+                0 -> {
+                    nums[current] = nums[redIndex]
+                    nums[redIndex] = 0
+                    redIndex++
+                    current++
+                }
+                1 -> {
+                    current++
+                }
+                2 -> {
+                    nums[current] = nums[blueIndex]
+                    nums[blueIndex] = 2
+                    blueIndex--
+                }
+            }
+        }
+        
+    }
+
+}
+
+
+fun main() {
+    val solution = Solution()
+    val input = intArrayOf(2, 0, 2, 1, 1, 0)
+    solution.sortColors(input)
+
+    println("Expected: [0, 0, 1, 1, 2, 2]")
+    println("Actual  : ${input.toList()}")
+}
